e545e013370020cbad00726f5c9b98d10cf893b2
HritaDutta <hritadutta@gmail.com>
2023-08-16 20:10:34 +0545

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Top from './Components/Top'
+
+function App() {
+  return (
+    <>
+    <Top/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Top.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Top() {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+export default Top
