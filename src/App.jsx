54bbd76251ee8b48731202bb01067664647ec953
HritaDutta <hritadutta@gmail.com>
2023-08-17 20:35:57 +0545

    finished top section with phone no., email and with social media logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Top from './Components/Top'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <>
     <Top/>
+    <Header/>
+    <Footer/>
     </>
   )
 }
--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 function Top() {
   return (
-    <h1>Hello</h1>
+    <section className='top '>
+      <Container className='text-color'>
+        <ul className='d-flex justify-content-between py-2'>
+          <div className='d-flex gap-4'>
+            <li><i class="bi bi-telephone-fill"></i> +7 777 123 1575 </li>
+            <li><i class="bi bi-envelope-fill"></i> admin@real-web.pro </li>
+          </div>
+
+          <div className='d-flex gap-4'>
+            <li><i class="bi bi-envelope-fill"></i></li>
+            <li><i class="bi bi-google"></i></li>
+            <li><i class="bi bi-twitter"></i></li>
+            <li><i class="bi bi-instagram"></i></li>
+          </div>
+        </ul>
+      </Container>
+    </section>
   )
 }
 
