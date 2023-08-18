67c4934950f738d329ea933d725a2286e195945c
HritaDutta <hritadutta@gmail.com>
2023-08-18 19:49:20 +0545

    finished design of header section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import logo from "../assets/Img/logo.png"
 
 function Header() {
   return (
-    <div>Header</div>
+    <>
+    <section className='container py-3'>
+      <Row className='align-items-center'>
+        <Col lg={2}>
+        <img src={logo} alt="" />
+        </Col>
+        <Col lg={8}>
+
+        </Col>
+        <Col lg={2} className=''>
+        <ul className='d-flex justify-content-end gap-4'>
+          <li className='d-flex gap-1'><i class="bi bi-suit-heart-fill"></i>Whislist(1)</li>
+          <li className='d-flex gap-1'><i class="bi bi-bar-chart-line-fill"></i>Compare(5)</li>
+          <li className='d-flex gap-1'><i class="bi bi-lock-fill"></i>Registration/Login</li>
+          <li className='d-flex gap-1'><i class="bi bi-cart-fill"></i>Cart(0)</li>
+        </ul>
+        </Col>
+      </Row>
+    </section>
+    </>
   )
 }
 
--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -3,15 +3,16 @@ import { Container } from 'react-bootstrap'
 
 function Top() {
   return (
-    <section className='top '>
-      <Container className='text-color'>
-        <ul className='d-flex justify-content-between py-2'>
+    <section className='top'>
+      <Container className='text-color '>
+        <ul className='d-flex justify-content-between py-2' >
           <div className='d-flex gap-4'>
             <li><i class="bi bi-telephone-fill"></i> +7 777 123 1575 </li>
             <li><i class="bi bi-envelope-fill"></i> admin@real-web.pro </li>
           </div>
 
           <div className='d-flex gap-4'>
+            <li><i class="bi bi-facebook"></i></li>
             <li><i class="bi bi-envelope-fill"></i></li>
             <li><i class="bi bi-google"></i></li>
             <li><i class="bi bi-twitter"></i></li>
