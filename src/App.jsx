df6c4db9bd8c778fb87e65b417f1729bef32248a
HritaDutta <hritadutta@gmail.com>
2023-08-18 20:28:33 +0545

    Added dropdown in All Catalogue

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Top from './Components/Top'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import  Navbar  from './Components/Navbar'
 
 function App() {
   return (
     <>
     <Top/>
     <Header/>
+    <Navbar/>
     <Footer/>
     </>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <section>
+        <Container className='border'>
+            <Row>
+                <Col lg={3}>
+                <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </div>
+                </Col>
+                <Col lg={1}>dafef</Col>
+                <Col lg={8}>dased</Col>
+            </Row>
+        </Container>
+    </section>
+    </>
   )
 }
 
